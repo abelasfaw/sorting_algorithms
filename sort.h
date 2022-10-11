91308b42f027ae701436a9b240d613df84857c8a
abelasfaw <abeloman89@gmail.com>
2022-10-11 09:20:08 -0700

    added task 0

---
+++ sort.h
@@ -0,0 +1,17 @@
+#ifndef SORT_HEADER
+#define SORT_HEADER
+/**
+ * struct listint_ - double linked list node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+#endif
